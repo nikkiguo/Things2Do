ba7df878f5b20be9681ae92d8d091104d53eb95f
dannywuwu <d246wu@gmail.com>
2023-01-14 17:49:57 -0500

    passing off frontend to nikki

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,11 @@ import "./App.css";
 
 function App() {
   const [response, setResponse] = useState(0);
+  const [timelimit, setTimelimit] = useState(0);
+  const [location, setLocation] = useState({
+    name: "Campus Services Bldg, 1280 Main St W Building Room 102, Hamilton, ON L8S 4L8",
+    coordinates: [43.26225, -79.92025],
+  });
 
   const request = () => {
     fetch("http://127.0.0.1:8080/")
@@ -15,8 +20,38 @@ function App() {
       });
   };
 
+  const finalizeGroup = () => {};
+
   return (
     <div className="App">
+      <h1>Location: {location.name}</h1>
+      <h2>Coordinates: {location.coordinates.join(", ")}</h2>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Time Limit (hours)
+        </label>
+        <br></br>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="1"
+        />
+      </div>
+
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Create Group
+      </button>
+      <br />
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={finalizeGroup}
+      >
+        Finalize Group
+      </button>
       <p onClick={request}>click</p>
       <p>{JSON.stringify(response)}</p>
     </div>
@@ -24,3 +59,26 @@ function App() {
 }
 
 export default App;
+
+/*
+1. Host Page
+## choose location (coordinates?)
+map choose -> coordinates -> backend
+
+## time limit
+
+## create group and link
+
+---
+
+## lock in button -> calculate the goods
+
+2. user page (localhost:8080/UUID)
+## choose an OSM category
+
+*/
+
+/*
+TODO
+- finalize plan button, hide until group create
+*/
